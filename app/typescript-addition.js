9bccb41332c899ce46c20c1cd20c1474e44fc3f5
Gyles Batara <gyles.batara@live.com>
2017-12-05 08:20:05 -0800

    Working prototype

---
+++ app/typescript-addition.js
@@ -0,0 +1,13 @@
+// var firstNumber: number = parseInt(prompt('Please enter a number.'));
+// var secondNumber: number = parseInt(prompt('Enter another number.'));
+// var sum: number = firstNumber + secondNumber;
+// alert("The sum of your two numbers is: " + sum);
+
+var findSum = function (firstValue, secondValue) {
+  var sum = firstValue + secondValue;
+  alert("The sum of your two numbers is " + sum);
+};
+
+var firstNumber = parseInt(prompt('Please enter a number.'));
+var secondNumber = parseInt(prompt('Enter another number.'));
+findSum(firstNumber, secondNumber);
